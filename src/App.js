999333860a72b92d29450829af830998cf964eff
Deluxateer <deluxateer@gmail.com>
2018-08-23 16:12:48 -0400

    adds component structure and reactstrap

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Button } from 'reactstrap';
+import Navbar from './Components/Navbar';
+import Summary from './Components/Summary';
+import Favorites from './Components/Favorites';
+import Submition from './Components/Submition';
+import Feed from './Components/Feed';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <React.Fragment>
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <footer>
+    <div class="container">
+      <ul class="list-inline">
+        <li class="list-inline-item"><a href="#">About Us</a></li>
+        <li class="list-inline-item"><a href="#">Privacy</a></li>
+        <li class="list-inline-item"><a href="#">Terms</a></li>
+        <li class="list-inline-item"><a href="#">Jobs</a></li>
+        <li class="list-inline-item"><a href="#">API</a></li>
+      </ul>
+      <div class="legal">
+        <p>&copy;CheckOut. All rights reserved.</p>
       </div>
+    </div>
+  </footer>
+      </React.Fragment>
     );
   }
 }
